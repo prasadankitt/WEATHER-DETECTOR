8389e1cd5522e325d1cb17edb41b7680665aad08
ANKiT <ankit768130@gmail.com>
2022-05-15 19:31:20 +0530

    first

---
+++ detectorapp/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+import json
+import urllib.request
+
+# Create your views here.
+def index(request):
+    if request.method == 'POST':
+        city = request.POST['city']
+        result = urllib.request.urlopen('http://api.openweathermap.org/data/2.5/weather?q='+city+'&appid=8952a6cd9ff5183314dfea5994d2c66a').read()
+        json_data = json.loads(result) 
+        data = {
+            "country_code": str(json_data['sys']['country']),
+            "coordinate": str(json_data['coord']['lon']) + ' ' + str(json_data['coord']['lat']),
+            "temp": str(json_data['main']['temp'])+'K',
+            "pressure": str(json_data['main']['pressure']),
+            "humidity": str(json_data['main']['humidity']),
+        }
+    else:
+        city = ''
+        data = {}
+    return render(request, 'index.html', {'city': city, 'data':data})
